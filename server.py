616b535c3d087f2168f190f50a4d9cb999c20ac3
Sam Allen <samallen659@gmail.com>
2024-07-18 19:37:06 +0100

    server built

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import platform
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("./index.html")
+def home():
+    hostname = platform.node()
+    return render_template("index.html", hostname=hostname)
